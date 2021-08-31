258b93c97e904a70cf136fc8f888eff227ca0088
Matias Vidoni <matiasrmontenegro@gmail.com>
2021-08-31 11:25:42 -0300

    Initial Commit

---
+++ ToDoIt/ToDoItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoItApp.swift
+//  ToDoIt
+//
+//  Created by Mati Montenegro on 31/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
